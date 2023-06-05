13395f03cac70b104affe128fa32fdfc5101f081
Oleg Khvostenko <xboctehko@yandex.ru>
2023-06-05 14:40:23 +0200

    WebPack fixed

--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,5 @@
 import Game from './game.js';
-// import GameSavingData, { readGameSaving as loadGame, writeGameSaving as saveGame } from './game_saving.js';
+import GameSavingData, { readGameSaving as loadGame, writeGameSaving as saveGame } from './game_saving.js';
 // import Bowman from './characters/bowman.js';
 // import PropertyList from './propery_list.js';
 // import GetSpecial from './get_special.js';
