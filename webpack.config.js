d8e148b53b1c152ed4c243efb05ade1c7694a4a1
Oleg Khvostenko <xboctehko@yandex.ru>
2023-06-09 17:06:56 +0200

    Webpack plugins added

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,22 @@
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
 module.exports = {
   mode: 'production',
+  module: {
+    rules: [{
+      test: /\.css$/,
+      use: [ MiniCssExtractPlugin.loader, 'css-loader' ],
+    }],
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: '[name].css',
+    }),
+
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+      filename: './index.html',
+    }),
+  ],
 };
